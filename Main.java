354d32d0c3f49dfd472cf942245a8a9b73300178
jamesponwith <jponwith@sandiego.edu>
2017-11-09 16:28:03 -0800

    Base project

--- Main.java
+++ Main.java
@@ -3,6 +3,54 @@ package com.booya;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    }
+
+    Object getFirst() {
+
+    }
+
+    Object getLast() {
+
+    }
+
+    void add(Object value) {
+
+    }
+
+    void addAfter(int index, Object value) {
+
+    }
+
+    Object set(int index, Object newValue) {
+
+    }
+
+    int lastIndex(Object value) {
+
+    }
+
+    void removeAll(Object value) {
+
+    }
+
+    MyLinkedList<Object> clone() {
+
+    }
+
+    boolean equals(Object o) {
+
+    }
+
+    MyLinkedList<Object> split() {
+
+    }
+
+    void doubler() {
+
+    }
+
+    void sublist(int i, int j) {
+
     }
 }
